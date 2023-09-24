251af927accb4f228df8cef5e34f76e06629afe7
SanSanter <santer-kart@yandex.ru>
2023-09-24 12:52:20 +0300

    four.1

--- login.php
+++ login.php
@@ -4,9 +4,10 @@ if (isset($_COOKIE['userName'])) {
 }  
 $message = "";
 $users = [
-    ["name" => "SLChat", "login" => "SLChat", "password" => "1305"],
-    ["name" => "Santer", "login" => "Santer", "password" => "123"],
-    ["name" => "NeSanter", "login" => "NeSanter", "password" => "qwert"]
+    ["name" => "Александр", "login" => "SLChat", "password" => "1305"],
+    ["name" => "Santer", "login" => "Santer", "password" => "fgegrgthr"],
+    ["name" => "NeSanter", "login" => "NeSanter", "password" => "qwert"],
+    ["name" => "Test", "login" => "Test", "password" => "Test"]
 ];
 
 
@@ -19,12 +20,12 @@ if (isset($_POST["login"]) && isset($_POST["password"])) {
 if (!empty($login) && !empty($password)) {
     foreach ($users as $user) {
         if ($user["login"] == $login && $user["password"] == $password) {
-            setcookie("userName", $user["name"], time() + 60 * 60 * 1000000);
+            setcookie("userName", $user["name"], time() + 60 * 60 * 24);
             header("location: /");
             die;
         }
     }
-    $message = "Логин или пароль не правильны или не существует!";
+    $message = "Логин или пароль не правильны или не существует! Повторите попытку!";
 }
 
 ?>
@@ -34,7 +35,7 @@ if (!empty($login) && !empty($password)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
     <form class="login_form" action="login.php" method="POST">
